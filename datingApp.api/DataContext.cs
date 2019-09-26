2770f6e1e01bffdfee9e16743a17c958f10b3bed
Jim Petas <gdpetas@hotmail.com>
2019-09-26 19:38:48 +0300

    First commit

---
+++ datingApp.api/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using datingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.api
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ datingApp.api/Helpers/StringHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.api.Helpers
+{
+    //Extension Method for string
+    public static class StringHelper
+    {
+        public static string ChangeFirstLetterCase(this string inpuString)
+        {
+            if (inpuString.Length > 0)
+            {
+                char[] charArray = inpuString.ToCharArray();
+                charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+                return new string(charArray);
+            }
+            return inpuString;
+        }
+    }
+}
