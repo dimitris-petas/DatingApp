fab57e861d6a1420824e9f61de1c070dc1e14645
Jim Petas <gdpetas@hotmail.com>
2019-09-27 00:02:33 +0300

    User Register

---
+++ datingApp.api/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using datingApp.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.api.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<User> UserExists(string username);
+    }
+}
