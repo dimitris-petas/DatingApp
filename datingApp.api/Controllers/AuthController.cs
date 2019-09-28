e8b4ac7f87970aa381a4c4e02a300355b1ce243b
Jim Petas <gdpetas@hotmail.com>
2019-09-28 13:27:03 +0300

    Register user

---
+++ datingApp.api/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.api.Data;
+using datingApp.api.Dtos;
+using datingApp.api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datingApp.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.username = userForRegisterDto.username.ToLower();
+            if (await _authRepository.UserExists(userForRegisterDto.username))
+                return BadRequest("Username allready exists.");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.username
+            };
+
+            var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.password);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ datingApp.api/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.api.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string password { get; set; }
+        public string username { get; set; }
+    }
+
+}
